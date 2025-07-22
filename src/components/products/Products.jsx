3f1638e07901d1e9506c0bd88bdaea1be51c0033
XurshidovichBekzod <xurshidovichbekzod@gmail.com>
2025-07-22 14:43:06 +0500

    products uzgardi

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -7,7 +7,7 @@ const Products = ({ data }) => {
   return (
     <>
       <h1 className='text-center text-xl font-bold text-[40px] text-[#3A3A3A]'>Our Products</h1>
-      <div className='p-[50px] grid grid-cols-5 gap-3'>  
+      <div className='p-[50px] grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 container'>  
         {
           data?.map((products) => (
             <div key={products.id}>
