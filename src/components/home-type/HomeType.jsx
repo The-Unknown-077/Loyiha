42edab2fc6cbcaf502c40b8b63699e328c6d16f0
The-Unknown-077 <gamergamervich@gmail.com>
2025-07-23 18:08:53 +0500

    Media qilindi va hatolar togirlandi

--- src/components/home-type/HomeType.jsx
+++ src/components/home-type/HomeType.jsx
@@ -7,28 +7,28 @@ import img_3 from "./images/Mask Group (1).png"
 
 const HomeType = () => {
   return (
-    <section className='px-[29px] mb-[57px]'>
+        <section className='px-4 sm:px-6 lg:px-[29px] mb-[57px]'>
       <div className='container mt-[55px]'>
-        <div className="text-center mx-auto mb-[62px]">
-          <h3 className='font-bold text-[32px] text-[#333]'>Browse The Range</h3>
-          <p className='font-normal text-[20px] text-[#667]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <div className="text-center mx-auto mb-[62px] max-w-[600px]">
+          <h3 className='font-bold text-[28px] sm:text-[32px] text-[#333]'>Browse The Range</h3>
+          <p className='font-normal text-[18px] sm:text-[20px] text-[#667]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </div>
-        <div className="grid grid-cols-3 gap-[20px] w-full place-content-center">
-          <div className="max-h-[546px] max-w-[381px] h-full w-full overflow-hidden rounded-[7px]">
-            <img src={img_1} alt="" className='h-[400px] w-[480px] mb-[30px] duration-300 hover:scale-105' />
-            <p className='font-semibold text-[24px] text-center text-[#333]'>Dining</p>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-[20px] w-full items-center">
+          <div className="max-h-[546px] max-sm:h-[400px] w-full rounded-[7px]">
+            <img src={img_1} alt="" className='h-auto max-sm:h-[300px] w-full mb-[20px] duration-300 object-cover rounded-[7px]' />
+            <p className='font-semibold text-[22px] sm:text-[24px] text-center text-[#333]'>Dining</p>
           </div>
-          <div className="max-h-[546px] max-w-[381px] h-full w-full overflow-hidden rounded-[7px]">
-            <img src={img_2} alt="" className='h-[400px] w-[480px] mb-[30px] duration-300 hover:scale-105' />
-            <p className='font-semibold text-[24px] text-center text-[#333]'>Living</p>
+          <div className="max-h-[546px] max-sm:h-[400px] w-full rounded-[7px]">
+            <img src={img_2} alt="" className='h-auto max-sm:h-[300px] w-full mb-[20px] duration-300 object-cover rounded-[7px]' />
+            <p className='font-semibold text-[22px] sm:text-[24px] text-center text-[#333]'>Living</p>
           </div>
-          <div className="max-h-[546px] max-w-[381px] h-full w-full overflow-hidden rounded-[7px]">
-            <img src={img_3} alt="" className='h-[400px] w-[480px] mb-[30px] duration-300 hover:scale-105' />
-            <p className='font-semibold text-[24px] text-center text-[#333]'>Bedroom</p>
+          <div className="max-h-[546px] max-sm:h-[400px] w-full rounded-[7px]">
+            <img src={img_3} alt="" className='h-auto max-sm:h-[300px] w-full mb-[20px] duration-300 object-cover rounded-[7px]' />
+            <p className='font-semibold text-[22px] sm:text-[24px] text-center text-[#333]'>Bedroom</p>
           </div>
         </div>
       </div>
-    </section>  
+    </section> 
   )
 }
 
