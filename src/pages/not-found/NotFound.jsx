b349a859ba7c6be64e0544c3b7e0ad54f3191b49
Shahzod Bektorayev <sizning-haqiqiy-emailingiz@gmail.com>
2025-07-25 20:51:00 +0300

    notFound404

--- src/pages/not-found/NotFound.jsx
+++ src/pages/not-found/NotFound.jsx
@@ -4,10 +4,22 @@ import {  useNavigate } from "react-router-dom";
 const NotFound = () => {
     const navigate = useNavigate()
   return (
-    <div style={{ textAlign: "center" }}>
-      <h1>404 sahifa</h1>
-      <button onClick={()=> navigate("/")}>go home</button>
-      <button onClick={()=> navigate(-1)}>go back</button>
+    <div className="flex flex-col items-center justify-center  text-white text-center px-4 p-8">
+      <h1 className="text-9xl font-extrabold text-red-500 drop-shadow-md">404</h1>
+      <h2 className="mt-4 text-4xl font-bold text-gray-900">Page Not Found</h2>
+      <p className="mt-2 text-gray-900 text-lg">
+      Sorry, the page you are looking for does not exist.
+      </p>
+      <div className="mt-8 flex gap-4">
+
+        <button
+          onClick={() => navigate("/")}
+          className="px-6 py-3 rounded-xl bg-red-500 hover:bg-red-600 transition-colors shadow-lg hover:shadow-red-500/50 font-semibold">go Home</button>
+
+        <button
+          onClick={() => navigate(-1)}
+          className="px-6 py-3 rounded-xl bg-gray-700 hover:bg-gray-600 transition-colors shadow-md font-semibold">Go back</button>
+      </div>
     </div>
   );
 };
