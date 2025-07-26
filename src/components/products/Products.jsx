2ff20a56cf6ecfe04f83e4a949c6a00298c32752
The-Unknown-077 <gamergamervich@gmail.com>
2025-07-26 18:29:03 +0500

    shop qismi qilindi

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -7,7 +7,7 @@ import { useWishlist } from '../../zustand/useWishlist'
 import { useCart } from '../../zustand/useCart'
 import image from "./images/grocery-store.png"
 
-const Products = ({ data, text }) => {
+const Products = ({ data, text, limit, setLimit }) => {
 
   const { toggleWishlist, wishlist } = useWishlist()
   const { add, cart } = useCart()
@@ -15,8 +15,8 @@ const Products = ({ data, text }) => {
   const navigate = useNavigate()
 
   return (
-    <>
-      <h1 className='text-center text-xl font-bold text-[40px] text-[#3A3A3A]'>{text ? text : "Our Products"}</h1>
+    <section className='flex justify-center items-center flex-col'>
+      <h1 className='text-center text-xl font-bold text-[40px] text-[#3A3A3A]'>{text ? text : ""}</h1>
       <div className='p-[50px] grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 container'>
         {
           data?.map((products) => (
@@ -60,7 +60,8 @@ const Products = ({ data, text }) => {
           ))
         }
       </div>
-    </>
+      <button onClick={() => setLimit(limit + 12)} className='mx-auto mb-[68px] text-center duration-300 hover:bg-[#B88E2F] hover:text-[#fff] py-[12px] px-[78px] font-bold text-[16px] text-[#B88E2F] border-[1px] border-[#B88E2F] leading-[150%]'>Show More</button>
+    </section>
   )
 }
 
